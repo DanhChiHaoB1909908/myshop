e3b2040a59f617a7d92bc3b8a83b9cf296cbb181
DanhChiHaoB1909908 <haob1909908@student.ctu.edu.vn>
2022-10-24 22:18:29 +0700

    Luu san pham yeu thich len Realtime Database cua Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -76,10 +76,10 @@ class ProductsManager with ChangeNotifier {
   //   }
   // }
 
-  void toggleFavoriteStatus(Product product) {
-    final savedStatus = product.isFavorite;
-    product.isFavorite = !savedStatus;
-  }
+  // void toggleFavoriteStatus(Product product) {
+  //   final savedStatus = product.isFavorite;
+  //   product.isFavorite = !savedStatus;
+  // }
 
   // void deleteProduct(String id) {
   //   final index = _items.indexWhere((item) => item.id == id);
@@ -128,6 +128,15 @@ class ProductsManager with ChangeNotifier {
     if (!await _productsService.deleteProduct(id)) {
       _items.insert(index, existingProduct);
       notifyListeners();
+    }
+  }
+
+  Future<void> toggleFavoriteStatus(Product product) async {
+    final savedStatus = product.isFavorite;
+    product.isFavorite = !savedStatus;
+
+    if (!await _productsService.saveFavoriteStatus(product)) {
+      product.isFavorite = savedStatus;
     }
   }
 }
